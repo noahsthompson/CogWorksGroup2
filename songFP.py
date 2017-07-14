97b95c516f9552d13d548124da35f658de58204a
Noah Thompson <noah.scott.thompson@gmail.com>
2017-07-14 09:56:26 -0400

    Create songFP.py

---
+++ songFP.py
@@ -0,0 +1,53 @@
+class songFP:
+    """ Allows the user to identify, or "fingerprint" a song
+    --------------------------------------------------------
+
+    
+    """
+
+    def __init__(self):
+        '''initiates songFP class
+        each song in database is in the form, '''
+        self.songDatabase = {}
+
+    def addSong(self, song):
+        """
+        Adds a new song to the song database
+
+        """
+
+        pass
+
+    def deleteSong(self, song):
+        """
+        Deletes a song from the database  
+        """
+
+        pass
+
+    def listSongs(self):
+        """Lists all the songs in the database"""
+        pass
+
+    def listArtists(self):
+        """Lists all the artists in the database w/o repeats"""
+
+    def findPeaks():#Roger write this
+        """ """
+        
+        pass
+
+    def match():#Daniel write this
+        """ 
+        """
+        pass
+
+    def findSong():#will use match and findPeaks as helper functions
+        """ """
+        pass
+
+
+
+
+
+
