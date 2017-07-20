d6611e00e8d65730a4a640d5b9f11a415e9220f8
DanielFan <danielfanchs@gmail.com>
2017-07-20 14:05:05 -0400

    Update Week2capstone.py

--- Week2capstone.py
+++ Week2capstone.py
@@ -1,1 +1,18 @@
 
+%matplotlib notebook
+from camera import save_camera_config, take_picture
+import matplotlib.pyplot as plt
+
+class facial:
+    
+    def __init__(self):
+        self.database = []
+        pass
+    
+    def camera(self, port=0, exposure=0.2):
+        save_camera_config(port, exposure)
+        img_array = take_picture()
+        self.database.append(img_array)
+        
+ok = facial()
+ok.camera()
