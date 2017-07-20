a5c9951cd63610615ed3811d5a0e0760fe8ac175
Noah Thompson <noah.scott.thompson@gmail.com>
2017-07-20 14:44:40 -0400

    Update FaceDatabase.py

--- FaceDatabase.py
+++ FaceDatabase.py
@@ -1,8 +1,14 @@
+#%matplotlib notebook Uncomment to run in jupyter
+import matplotlib.pyplot as plt
+import skimage.io as io
 import numpy as np
+
+from camera import save_camera_config, take_picture
+from dlib_models import load_dlib_models
 
 class faceDatabase():
     """faceDatabase:
-        feilds:
+        fields:
         faceData- Tuple of (name(str), discriptorArray(np.array),weight(int)). Discriptor array has shape (128,). Weight
         is the number of images that have gone into the current average, and determines how much to weight the incoming
         averages.
@@ -16,14 +22,23 @@ class faceDatabase():
     def __init__(self,loadFile=None):
         if(loadFile=None):
             self.faceData=np.array([])
+            
         else:
             pass
-    def get_name(self,index)
-    return self.faceData[index,0]
-    def get_descrip(self,index)
-    return self.faceData[index,1]
-    def get_weight(self,index)
-    return self.faceData[index,2]
+        
+    def camera(self, port=0, exposure=0.2):
+        save_camera_config(port, exposure)
+        img_array = take_picture()
+        return img_array
+        
+    def get_name(self,index):
+        return self.faceData[index,0]
+    
+    def get_descrip(self,index):
+        return self.faceData[index,1]
+    
+    def get_weight(self,index):
+        return self.faceData[index,2]
     def add(self,name,discriptor):
         """ add: adds new face to database, or averages new discriptor with name already in database
             params:
@@ -57,3 +72,48 @@ class faceDatabase():
             return "cant find anyone close"
         else:
             return closest_person
+        
+    def camera(self, port=0, exposure=0.2):
+        save_camera_config(port, exposure)
+        img_array = take_picture()
+        self.database.append(img_array)
+        
+    def camera(self, port=0, exposure=0.2):
+        save_camera_config(port, exposure)
+        img_array = take_picture()
+        self.database.append(img_array)
+        
+    @load_dlib_models
+    def detect(self, img, upscale = 1):
+        '''
+        takes in:
+            -img: an array
+            -upscale: an integer that represents the
+            amount of times to upscale the img to detect
+            smaller faces
+
+        returns: a numpy array of dimensions, w, 128 with w being the number of faces
+        present in the image each one having a corresponding 128 size vector.
+
+        '''
+        from dlib_models import models
+
+        face_detect = models["face detect"]
+        face_rec_model = models["face rec"]
+        shape_predictor = models["shape predict"]
+
+        detections = face_detect(img, upscale)
+        detections = list(detections)
+
+        faceArr = np.zeros((len(detections), 128))
+
+
+        for it in range(len(detections)):
+            det = detections[it]
+            shape = shape_predictor(img, det)
+            descriptor = np.array(face_rec_model.compute_face_descriptor(img, shape))
+            faceArr[it] = descriptor
+
+        return faceArr
+    
+    
