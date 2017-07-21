8ba2c587a8a48b40632873376668575a48a00820
Noah Thompson <noah.scott.thompson@gmail.com>
2017-07-21 00:27:43 -0400

    It works now, but uses a list in place of np array
    we need to optimize and train more but this works pretty well
    should add labels to boxes as well

--- Week2capstone.py
+++ Week2capstone.py
@@ -1,5 +1,6 @@
 %matplotlib notebook
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import skimage.io as io
 import numpy as np
 
@@ -29,56 +30,64 @@ class faceDatabase():
     def camera(self, port=0, exposure=0.2):
         save_camera_config(port, exposure)
         img_array = take_picture()
+        if img_array == []:
+            raise ValueError("No face detected") 
         return img_array
+
+
         
-    def get_name(self,index):
+    def get_name(self,index):#DEAD
         return self.faceData[index,0]
     
-    def get_descrip(self,index):
+    def get_descrip(self,index):#DEAD
         return self.faceData[index,1]
     
-    def get_weight(self,index):
+    def get_weight(self,index):#DEAD
         return self.faceData[index,2]
+    
     def add(self,name,descriptor):
         """ add: adds new face to database, or averages new descriptor with name already in database
             params:
             name(str)- Name of new face
             descriptor(np.array)- (128,) array of descriptors"""
         # gets the array of names
+
         for face in self.faceData:
             if face[0] == name:
-
-                face[1] = (face[1]*face[2] + descriptor)/(face[2]+1)
+                face[1] = avgVectorIn(face[1], descriptor, face[2])
                 face[2] += 1
+                
+                return None
         self.faceData.append([name, descriptor, 1])
-        pass
+        return None
             
             
     def saveFile():
         pass
-    #database shape: np.array((name1, vector1), (name2, vector2), etc.)
+    #database shape: np.array((name1, vector1, weight1), (name2, vector2, weight2), etc.)
     def match(self, imgvector):
         #vector.shape: (128,)
         closest_person = "" #most similar person
-        minDist =109. #distance w/ most similar person
-        
-        for face in self.faceData: #look thru every face in db
+        minDist = .5 #distance w/ most similar person
+        #print(self.faceData)
+        for face in self.faceData: #look through every face in db
             l2 = np.sqrt(np.sum((face[1] - imgvector)**2))#distance between db face and the given face
+            print(l2)
             if (l2 < minDist): #if l2 is smaller
                 minDist = l2
                 closest_person = face[0]
-        if (minDist > 0.5):
+
+        if (minDist == 0.5):
             return "unknown"
         else:
-            print('probably, ')
-            print('l2, ', l2)
+            print('minDist: ', minDist)
             return closest_person
  
         
     def addCamera(self, name, port=0, exposure=0.2):
         save_camera_config(port, exposure)
         img_array = take_picture()
-        self.add(name,img_array)
+        self.add(name,self.img_to_array(img_array)[0])
         
 
         
@@ -103,34 +112,43 @@ class faceDatabase():
         detections = list(detections)
 
         faceArr = np.zeros((len(detections), 128))
-
+        
+        boxes = []
 
         for it in range(len(detections)):
             det = detections[it]
             shape = shape_predictor(img, det)
             descriptor = np.array(face_rec_model.compute_face_descriptor(img, shape))
             faceArr[it] = descriptor
-
-        return faceArr
+            l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
+            boxes.append((l,r,t,b))
+        return faceArr, boxes
     
     def detectFromImg(self, img, upscale = 1):
-        faces = self.img_to_array(img)
-        print(faces.shape)
+        faces = self.img_to_array(img)[0]
+        #print(faces.shape)
         people = []
         for face in faces:
             people.append(self.match(face))
-        return people
-'''   
+        return people, self.img_to_array(img)[1]
+    
+    def abracadabra(self):
+        y = test.camera()
+        people, borders = test.detectFromImg(y)
+        fig,ax = plt.subplots()
+        ax.imshow(y)
+        for border in borders:
+            ax.add_patch(patches.Rectangle((border[1], border[3]),border[0]-border[1],border[2]-border[3],edgecolor = 'red', fill=False))
+        print('The people in this picture are(in order):', ', '.join(people))
+        
+    
 test = faceDatabase()
-#print(test.img_to_array(io.imread("obama.jpg")))
-test.add('obama', test.img_to_array(io.imread("obama.jpg")))
-test.add('obama', test.img_to_array(io.imread("obama2.jpg")))
-test.add('noah', test.img_to_array(io.imread("noah_thompson1.jpg")))
-test.add('noah', test.img_to_array(io.imread("noah_thompson2.jpg")))
-test.add('noah', test.img_to_array(io.imread("noah_thompson3.jpg")))
-#test.addCamera('roger')
-#test.add('noah', test.img_to_array(io.imread("noah_thompson4.png")))
-#test.add('noah', test.img_to_array(io.imread("noah_thompson5.png")))
-test.detectFromImg(test.camera())
 
+'''test.add('obama', test.img_to_array(io.imread("obama.jpg"))[0])
+test.add('obama', test.img_to_array(io.imread("obama2.jpg"))[0])
+test.add('noah', test.img_to_array(io.imread("Noah_Thompson1.jpg"))[0])
+test.add('noah', test.img_to_array(io.imread("Noah_Thompson3.jpg"))[0])
 '''
+
+#test.addCamera('noah')
+#test.abracadabra()
