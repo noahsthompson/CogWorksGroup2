b9866bbf035531e69226d4a1dcc97ec0d5b4ad51
Sam Murray <sam@pmurray.com>
2017-07-28 14:52:41 -0400

    Added associateEntity.

--- EntityDatabase.py
+++ EntityDatabase.py
@@ -7,6 +7,8 @@ from nltk.tokenize import word_tokenize
 import numpy as np
 import string
 import time
+import itertools
+import hashlib
 def unzip(pairs):
     """Splits list of pairs (tuples) into separate lists.
     
@@ -25,18 +27,39 @@ class entityDatabase():
         #dictionary of counts 
         self.entity_relationships=defaultdict(Counter)
     def tokenize(self,text):
-        return nltk.word_tokenize(sentence)
+        """" Tokenizes text
+             param: text(str) - the string to be tokenized
+             return: (list(str) - the list of tokens)
+             """
+        return nltk.word_tokenize(text)
     def getEntities(self,words):
-        tokens=word_tokenize(words)
+        """Gets the list of entities in a text
+           Param: words(str/list(str)) - String or list of tokens to find the entities in
+           Return: (list(tuple(str)) - A list of tuples, where each tuple is a tokenized entity
+           """
+        if (isinstance(words,str)):
+            tokens=word_tokenize(words)
+        else:
+            tokens=words
         pos = nltk.pos_tag(tokens)
         named_entities = nltk.ne_chunk(pos, binary=True)
         entities=[]
         for i in range(0, len(named_entities)):
             ents = named_entities.pop()
+
             if getattr(ents, 'label', None) != None and ents.label() == "NE": 
-                entities.append(tuple([ne[0] for ne in ents]))
+                entities.append(tuple([(ne[0].lower()) for ne in ents]))
         return entities
-    def add(self,doc,id):
+    
+    def add(self,doc,id=None):
+        """Adds all entities in doc to database, relates each entity to document id and each entity in the doc to each other
+            Params: doc(str)- The document to be added
+                    id(str)(optional, default=None)- Unique id for document in database. If none, database will generate a 
+                    SHA224 hash on the document for the id.
+            Returns: None
+            """
+        if id == None:
+            id=hash(doc)
         if id in self.id_entity_count.keys():
             raise Exception("Already In Database")
         entities=self.getEntities(doc)
@@ -51,21 +74,90 @@ class entityDatabase():
         
             
     def remove(self,id):
+        """Removes all traces of doc from database
+            Params: id(str)-id of document to be removed.
+            Returns: None
+            """
         if not(id in self.id_entity_count.keys()):
             raise Exception("Not In Database")
-        entities=(self.id_entity_count[id].keys())
+        entities=list(self.id_entity_count[id].keys())
+        print(entities)
         for entity in entities:
             self.entity_doc[entity].remove(id)
-    def encode(self, entities, code):
-        c=[]
-        for entity in entities:
-            c.append(code[entity])
-        return c
-
+            for e in entities:
+                if(e != entity):
+                    self.entity_relationships[entity][e]-=1
+                    if self.entity_relationships[entity][e] == 0:
+                        self.entity_relationships[entity].pop(e)
+            if len(self.entity_relationships[entity]) ==0:
+                self.entity_relationships.pop(entity)
+            if len(self.entity_doc[entity]) == 0:
+                self.entity_doc.pop(entity)
+        
+    
     def associateEntity(self,entity,k=None):
+        """Associates entity in database to entitys that appear in a similar context.
+           Params:entity(str/tuple(str))- target entity. Can either be a string with incorrect capitalizeation(assumes full
+                  string is one entity) or an entity(a tuple of strings) with correct capitalization.
+                  k(int)- Number of entitys returned, in order of most related to least related. If none, return all
+                  relations. Will not return entitys with relation score equal to 0.
+           Return:(tuple(list(tuple(str),bool)- Returns a tuple containing a list of related entitys(tuples of strings) and a
+                   boolean indicating whether the association failed. The association is "failed" if the target entity is not
+                   found in the database, in which case the function will return a list of close matches(entitys that contain)
+                   elements similar to the target entity (ie if the target entity ("Donald") is not found in the database, but
+                   the database does contain entities ("Donald","Trump") and ("Donald", "Glover") the function will return 
+                   ([("Donald","Trump"),("Donald","Glover")],False), indicating it has failed to find a perfect match, but
+                   managed to find 2 partial matches. If the database contained a reference to ("Donald"), it will return
+                   ([**list of related entitys**],True)).
+           Notes: This function is basically a suped up wrapper for getRelatedEntity, made specifically for integration with
+           Alexia.(faulty abstraction)
+           """
+           
+        if(isinstance(entity,str)):
+            tokens=self.tokenize(entity)
+            tokens=[tokens[i].lower().capitalize() for i in range(len(tokens))]
+            entity=self.getEntities(tokens)[0]
+        else:
+            entity=tuple([e.lower() for e in entity])
+        print(entity)
+        
+        entities=list(self.entity_relationships.keys())
+        if entity in entities:
+            return (self.getRelatedEntity(entity,k),True)
+        else:
+            out=[]
+            for e in entities:
+                for token in entity:
+                    if token in e and not(e in out):
+                        out.append(e)
+            return (out,False)
+            
+    def getRelatedEntity(self,entity,k=None):
+        """Takes an entity, finds related entities
+            Params: Entity(tuple(str))- Target entity
+                    k(int)(optional,default=None)- Number of entitys returned, in order of most related to least related. If
+                    none, return all relations. Will not return entitys with relation score equal to 0.
+                    """
+        if not(entity in self.entity_relationships.keys()):
+            raise Exception("Entity Not In Database")
         related=self.entity_relationships[entity].most_common(k)
-        return unzip(related)[0]
+        return [i[0] for i in list(itertools.filterfalse(lambda x:x[1]<=0,related))]
+        
     def associateTopic(self,topic,engine,context=5,out=5):
+        """Gets entities related to a topic
+            Params: topic(str)- topic to relate to entities.
+                    engine(searchEngine)- engine to search topic on. Document Ids returned will be cross referenced with entity
+                    database, and the most referenced entity across the documents will be returned.
+                    context(int)(Optional, default=5)- The number of entitys per document (ranked by frequency) to be
+                    considered
+                    out(int)(Optional, default=5)- The number of entities to be returned, ranked by how much each occurs across
+                    the documents
+            Returns: list(tuple(str))- list of related entitys
+            Notes: Algorithim needs to be tweeked, a document with an overwhelming amount of one entity can unfairly shift
+            balances
+            """
+            
+        #TODO: TEST THIS
         ids=engine.query(topic)
         totalCount=Counter()
         for id in ids:
